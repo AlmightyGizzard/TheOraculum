63e2ae707a375728fcb434f16191f20edeaa44cf
Dominic Cohen (labs) <dominicohen+1@gmail.com>
2022-02-28 20:30:23 +0000

    Created TextSwitch
    Foundations for viewing Text

---
+++ Assets/Scripts/TextSwitch.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextSwitch : Interactable
+{
+    public override string GetDescription(){
+        return "Hold [E] to read the passage.";
+    }
+}
