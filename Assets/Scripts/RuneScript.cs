3ac337719b02030c78d1f0ef81a613c6bd958bc4
Dominic Cohen <dominicohen+1@gmail.com>
2022-03-06 14:58:10 +0000

    RuneScript Updates
    Added RuneScript to the new Interactable class, allowing the player to modify its contents.

--- Assets/Scripts/RuneScript.cs
+++ Assets/Scripts/RuneScript.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class RuneScript : MonoBehaviour
+public class RuneScript : Interactable
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshPro text;
+    public float glow = 0f;
+    public float glowDecay = 0.05f;
+    string st = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public int currentLetter; 
+
+    public override string GetDescription()
     {
-        
+        if(glow <= 0.1f)
+        {
+            glow += glowDecay;
+        }
+        return "Press [E] to scrawl.";
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Interact()
     {
-        
+        Debug.Log("Reading!");
+        if(currentLetter >= st.Length-1)
+        {
+            currentLetter = 0;
+        }
+        else
+        {
+            currentLetter++;
+        }
+        text.text = st[currentLetter].ToString();
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        char value = char.Parse(text.text);
+        currentLetter = st.IndexOf(value);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        text.outlineWidth = glow;
+        if(glow >= 0)
+        {
+            glow -= glowDecay;
+        }
     }
 }
