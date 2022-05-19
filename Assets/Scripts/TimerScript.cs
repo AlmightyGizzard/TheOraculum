3fead2cf9ed92895ac987b0483eaf3e26b7048f1
Dominic (Labs) <dominicohen+1@gmail.com>
2022-05-19 14:59:43 +0100

    Light integration
    The timer now switches off light objects in the correct sequence, with the precise timing tuned to the number of lights in any given set. Needs support for multiple light sets.

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -6,8 +6,20 @@ using TMPro;
 
 public class TimerScript : MonoBehaviour
 {
-    float countdown = 30.0f;
+    public float dayLength; 
     public TextMeshProUGUI text;
+    [SerializeField]
+    float countdown;
+    float nightLength;
+
+    public List<GameObject> lightsDorms;
+
+    private void Awake()
+    {
+        countdown = dayLength;
+        float halfday = dayLength / 2;
+        nightLength = dayLength / 2;
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,7 +29,25 @@ public class TimerScript : MonoBehaviour
             countdown -= Time.deltaTime;
             double b = System.Math.Round(countdown, 2);
             text.text = b.ToString();
+
+            // We want an each light going out in sequence at a regular pace
+            float timePerLight = (nightLength/2) / lightsDorms.Count;
+
+            // Check if each light should be off. 
+            for(int i = lightsDorms.Count; i > 0; i--)
+            {
+                // If the timer is less than the amount of time per light multiplied by its position in the list, 
+                // turn it off. Half of the nightLength is added here so that it all happens in the 3rd quarter of the day,
+                // leaving a period of time where all lights are off. 
+                if(countdown < (i * timePerLight)+(nightLength/2))
+                {
+                    lightsDorms[i - 1].SetActive(false);
+                }
+            }
+
         }
+
+        
         
         if(countdown < 0)
         {
