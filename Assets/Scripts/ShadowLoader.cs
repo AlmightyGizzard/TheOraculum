2910d505108a28e1f2b16323efd9f40f0aa6b4b2
Dominic (labs) <dominicohen+1@gmail.com>
2022-05-21 17:01:41 +0100

    Shadow Optimisations
    Created a script that turns off shadowcasting should the object in question be off screen - should save on performance especially when we start adding in more bookshelves.

---
+++ Assets/Scripts/ShadowLoader.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+
+
+public class ShadowLoader : MonoBehaviour
+{
+    private TopDownController player;
+    private CompositeShadowCaster2D shadowGroup;
+    private float cutOff = 12;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<TopDownController>();
+        shadowGroup = GetComponent<CompositeShadowCaster2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        shadowGroup.enabled = true;
+        if(Vector3.Distance(player.transform.position, transform.position) > cutOff)
+        {
+            shadowGroup.enabled = false;
+        }
+    }
+}
